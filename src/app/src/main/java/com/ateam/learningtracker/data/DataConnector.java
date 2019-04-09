eb062ead8ae81451a24cca9340a8772a10fe5c89
Kuky <Kukant96@gmail.com>
2019-04-09 16:55:58 +0100

    GetSubjectsProgressInfo method

--- src/app/src/main/java/com/ateam/learningtracker/data/DataConnector.java
+++ src/app/src/main/java/com/ateam/learningtracker/data/DataConnector.java
@@ -1,5 +1,27 @@
 package com.ateam.learningtracker.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataConnector {
+    public static List<SubjectProgress> getSubjectsProgressInfo() {
+        List<SubjectEntity> allSubjects = SubjectEntity.listAll(SubjectEntity.class);
+        List<SubjectProgress> subjectProgresses = new ArrayList<SubjectProgress>();
+
+        for (SubjectEntity subject:allSubjects) {
+            SubjectProgress sp = new SubjectProgress();
+            sp.name = subject.name;
+            sp.overallProgress = getSubjectProgress(subject);
+
+            subjectProgresses.add(sp);
+        }
+        return subjectProgresses;
+    }
+
+    private static float getSubjectProgress(SubjectEntity subject) {
+        // TODO
+        return 0f;
+    }
+
 
 }
---
+++ src/app/src/main/java/com/ateam/learningtracker/data/SubjectProgress.java
@@ -0,0 +1,7 @@
+package com.ateam.learningtracker.data;
+
+public class SubjectProgress {
+    public String name;
+    public float overallProgress;
+}
+
